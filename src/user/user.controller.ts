1e991124346de9c76f55202f83d07295b3e54185
mel-ak <melakesisay@gmail.com>
2024-12-13 09:52:57 +0300

    Test fixed

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserModule } from './user.module';
 
 @Controller('user')
+@UseInterceptors(UserModule)
 export class UserController {
   constructor(private readonly userService: UserService) {}
   // GET route to return dummy JSON
